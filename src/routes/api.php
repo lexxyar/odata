d87c0ad2581b5ef8cf6110c95ddecd9a94154fb3
Aleksey Komov <lexxyar@ya.ru>
2021-03-17 14:17:45 +0300

    added middleware auth:api to routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,22 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
-//    Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('/{any}', function (Request $request) {
-        $odata = new \LexxSoft\odata\Odata($request);
-        return $odata->response();
-    })->where('any', '^(?!api).*$');
-    Route::post('/{any}', function (Request $request) {
-        $odata = new \LexxSoft\odata\Odata($request);
-        return $odata->response();
-    })->where('any', '^(?!api).*$');
-    Route::put('/{any}', function (Request $request) {
-        $odata = new \LexxSoft\odata\Odata($request);
-        return $odata->response();
-    })->where('any', '^(?!api).*$');
-    Route::delete('/{any}', function (Request $request) {
-        $odata = new \LexxSoft\odata\Odata($request);
-        return $odata->response();
-    })->where('any', '^(?!api).*$');
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('/{any}', function (Request $request) {
+            $odata = new \LexxSoft\odata\Odata($request);
+            return $odata->response();
+        })->where('any', '^(?!api).*$');
+        Route::post('/{any}', function (Request $request) {
+            $odata = new \LexxSoft\odata\Odata($request);
+            return $odata->response();
+        })->where('any', '^(?!api).*$');
+        Route::put('/{any}', function (Request $request) {
+            $odata = new \LexxSoft\odata\Odata($request);
+            return $odata->response();
+        })->where('any', '^(?!api).*$');
+        Route::delete('/{any}', function (Request $request) {
+            $odata = new \LexxSoft\odata\Odata($request);
+            return $odata->response();
+        })->where('any', '^(?!api).*$');
+    });
 });
-//});
