04f892de753ae9825f0ad941b619bf1d5c183cd8
Aleksey Komov <lexxyar@ya.ru>
2021-08-12 16:49:34 +0300

    Metadata output structure changed

--- src/Primitives/IsRestable.php
+++ src/Primitives/IsRestable.php
@@ -18,154 +18,154 @@ use ReflectionMethod;
  */
 trait IsRestable
 {
-    /**
-     * @var bool
-     */
-    public $isRestModel = true;
+  /**
+   * @var bool
+   */
+  public $isRestModel = true;
 
-    /**
-     * @var array
-     */
-    private $fields = [];
+  /**
+   * @var array
+   */
+  private $fields = [];
 
-    /**
-     * @return array
-     */
-    public function getFields(): array
-    {
-        $this->describeFields();
-        return $this->fields;
+  /**
+   * @return array
+   */
+  public function getFields(): array
+  {
+    $this->describeFields();
+    return $this->fields;
+  }
+
+  /**
+   * Валидирование данных
+   * @return bool
+   * @throws \Exception
+   */
+  public function validateObject()
+  {
+    // ToDo Validate maximum length
+    $keyField = $this->getKeyName();
+
+    // Проверка на обязательные поля
+    if (isset($this->required) && is_array($this->required)) {
+      foreach ($this->required as $field) {
+        if (!isset($this->$field)) {
+          throw new \Exception('Fill `' . $field . '` field');
+        }
+        if (trim($this->$field) == '') {
+          throw new \Exception('Field `' . $field . '` cannot be empty');
+        }
+      }
     }
 
-    /**
-     * Валидирование данных
-     * @return bool
-     * @throws \Exception
-     */
-    public function validateObject()
-    {
-        // ToDo Validate maximum length
-        $keyField = $this->getKeyName();
+    // Проверка уникальности значений
+    if (isset($this->unique) && is_array($this->unique)) {
+      foreach ($this->unique as $field) {
+        $value = null;
+        if (isset($this->$field)) {
+          $value = $this->$field;
+        }
+        if ($this->where($field, $value)->where($keyField, '<>', $this->$keyField)->exists()) {
+          throw new \Exception('Value `' . $value . '` of `' . $field . '` is not unique');
+        }
+      }
+    }
+    return true;
+  }
 
-        // Проверка на обязательные поля
-        if (isset($this->required) && is_array($this->required)) {
-            foreach ($this->required as $field) {
-                if (!isset($this->$field)) {
-                    throw new \Exception('Fill `' . $field . '` field');
-                }
-                if (trim($this->$field) == '') {
-                    throw new \Exception('Field `' . $field . '` cannot be empty');
-                }
+  /**
+   * Выбор связей, описанных в модели
+   * @return array
+   * @throws \ReflectionException
+   */
+  public function relationships()
+  {
+    $model = new static;
+    $reflector = new ReflectionClass($model);
+
+    $relations = [];
+    foreach ($reflector->getMethods() as $reflectionMethod) {
+
+      if ($reflectionMethod instanceof ReflectionMethod
+        && $reflectionMethod->isPublic()
+        && empty($reflectionMethod->getParameters())
+        && $reflectionMethod->getName() !== __FUNCTION__
+      ) {
+
+        $modelStartsWith = substr($reflectionMethod->getDeclaringClass()->getName(), 0, strlen('App\\Models\\'));
+        if ($modelStartsWith == 'App\\Models\\') {
+          $model = $model->first();
+          if (in_array($reflectionMethod->name, ['forceDelete', 'getFields'])) {
+            continue;
+          }
+          $return = $reflectionMethod->invoke($model);
+
+          if ($return instanceof Relation) {
+
+            $modelStartsWith = substr((new ReflectionClass($return->getRelated()))->getName(), 0, strlen('App\\Models\\'));
+
+            if ($modelStartsWith !== 'App\\Models\\') {
+              continue;
             }
-        }
 
-        // Проверка уникальности значений
-        if (isset($this->unique) && is_array($this->unique)) {
-            foreach ($this->unique as $field) {
-                $value = null;
-                if (isset($this->$field)) {
-                    $value = $this->$field;
-                }
-                if ($this->where($field, $value)->where($keyField, '<>', $this->$keyField)->exists()) {
-                    throw new \Exception('Value `' . $value . '` of `' . $field . '` is not unique');
-                }
+            $ownerKey = null;
+            if ((new ReflectionClass($return))->hasMethod('getOwnerKey'))
+              $ownerKey = $return->getOwnerKey();
+            else {
+              $segments = explode('.', $return->getQualifiedParentKeyName());
+              $ownerKey = $segments[count($segments) - 1];
             }
+
+            $rel = new OdataRelationship([
+              'name' => $reflectionMethod->getName(),
+              'type' => (new ReflectionClass($return))->getShortName(),
+              'model' => (new ReflectionClass($return->getRelated()))->getName(),
+              'foreignKey' => (new ReflectionClass($return))->hasMethod('getForeignKeyName')
+                ? $return->getForeignKeyName()
+                : $return->getForeignPivotKeyName(),
+              'ownerKey' => $ownerKey,
+              'fkName' => (new ReflectionClass($return->getRawBindings()))->hasMethod('getTable')
+                ? $return->getRawBindings()->getTable()
+                : '',
+              'snakeName' => OdataHelper::toSnakeCase($reflectionMethod->getName()),
+            ]);
+            $relations[] = $rel;
+          }
         }
-        return true;
+      }
     }
+    return $relations;
+  }
 
-    /**
-     * Выбор связей, описанных в модели
-     * @return array
-     * @throws \ReflectionException
-     */
-    public function relationships()
-    {
-        $model = new static;
-        $reflector = new ReflectionClass($model);
+  /**
+   * Составляет описание полей на основе БД
+   */
+  private function describeFields()
+  {
+    if (sizeof($this->fields) > 0) return;
 
-        $relations = [];
-        foreach ($reflector->getMethods() as $reflectionMethod) {
-
-            if ($reflectionMethod instanceof ReflectionMethod
-                && $reflectionMethod->isPublic()
-                && empty($reflectionMethod->getParameters())
-                && $reflectionMethod->getName() !== __FUNCTION__
-            ) {
-
-                $modelStartsWith = substr($reflectionMethod->getDeclaringClass()->getName(), 0, strlen('App\\Models\\'));
-                if ($modelStartsWith == 'App\\Models\\') {
-                    $model = $model->first();
-                    if ( in_array($reflectionMethod->name, ['forceDelete', 'getFields'])){
-                        continue;
-                    }
-                    $return = $reflectionMethod->invoke($model);
-
-                    if ($return instanceof Relation) {
-
-                        $modelStartsWith = substr((new ReflectionClass($return->getRelated()))->getName(), 0, strlen('App\\Models\\'));
-
-                        if ($modelStartsWith !== 'App\\Models\\') {
-                            continue;
-                        }
-
-                        $ownerKey = null;
-                        if ((new ReflectionClass($return))->hasMethod('getOwnerKey'))
-                            $ownerKey = $return->getOwnerKey();
-                        else {
-                            $segments = explode('.', $return->getQualifiedParentKeyName());
-                            $ownerKey = $segments[count($segments) - 1];
-                        }
-
-                        $rel = new OdataRelationship([
-                            'name' => $reflectionMethod->getName(),
-                            'type' => (new ReflectionClass($return))->getShortName(),
-                            'model' => (new ReflectionClass($return->getRelated()))->getName(),
-                            'foreignKey' => (new ReflectionClass($return))->hasMethod('getForeignKeyName')
-                                ? $return->getForeignKeyName()
-                                : $return->getForeignPivotKeyName(),
-                            'ownerKey' => $ownerKey,
-                            'fkName' => (new ReflectionClass($return->getRawBindings()))->hasMethod('getTable')
-                                ? $return->getRawBindings()->getTable()
-                                : '',
-                            'snakeName' => OdataHelper::toSnakeCase($reflectionMethod->getName()),
-                        ]);
-                        $relations[] = $rel;
-                    }
-                }
-            }
-        }
-        return $relations;
+    $raw = DB::select(DB::raw("DESCRIBE " . $this->getTable()));
+    $this->fields = [];
+    foreach ($raw as $field) {
+      $this->fields[] = new OdataFieldDescription($field);
     }
+  }
 
-    /**
-     * Составляет описание полей на основе БД
-     */
-    private function describeFields()
-    {
-        if (sizeof($this->fields) > 0) return;
-
-        $raw = DB::select(DB::raw("DESCRIBE " . $this->getTable()));
-        $this->fields = [];
-        foreach ($raw as $field) {
-            $this->fields[] = new OdataFieldDescription($field);
-        }
+  /**
+   * Проверяет наличие поля
+   *
+   * @param string $sField
+   * @return bool
+   */
+  public function hasField(string $sField): bool
+  {
+    $res = false;
+    foreach ($this->getFields() as $field) {
+      if ($field instanceof OdataFieldDescription) {
+        if ($field->getName() == $sField) return true;
+      }
     }
-
-    /**
-     * Проверяет наличие поля
-     *
-     * @param string $sField
-     * @return bool
-     */
-    public function hasField(string $sField): bool
-    {
-        $res = false;
-        foreach ($this->getFields() as $field) {
-            if ($field instanceof OdataFieldDescription) {
-                if ($field->getName() == $sField) return true;
-            }
-        }
-        return $res;
-    }
+    return $res;
+  }
 }
--- src/Primitives/OdataRelationship.php
+++ src/Primitives/OdataRelationship.php
@@ -9,56 +9,55 @@ namespace LexxSoft\odata\Primitives;
  */
 class OdataRelationship
 {
-    /**
-     * @var string
-     */
-    public $name;
+  /**
+   * @var string
+   */
+  public $name;
 
-    /**
-     * @var mixed
-     */
-    public $type;
+  /**
+   * @var mixed
+   */
+  public $type;
 
-    /**
-     * @var mixed
-     */
-    public $model;
+  /**
+   * @var mixed
+   */
+  public $model;
 
-    /**
-     * @var mixed
-     */
-    public $foreignKey;
+  /**
+   * @var mixed
+   */
+  public $foreignKey;
 
-    /**
-     * @var mixed
-     */
-    public $ownerKey;
+  /**
+   * @var mixed
+   */
+  public $ownerKey;
 
-    /**
-     * @var string
-     */
-    public $fkName;
+  /**
+   * @var string
+   */
+  public $fkName;
 
-    /**
-     * @var string
-     */
-    public $snakeName;
+  /**
+   * @var string
+   */
+  public $snakeName;
 
-    /**
-     * OdataRelationship constructor.
-     * @param array $relationship
-     */
-    public function __construct($relationship = [])
-    {
-        if ($relationship)
-        {
-            $this->name = $relationship['name'];
-            $this->type = $relationship['type'];
-            $this->model = $relationship['model'];
-            $this->foreignKey = $relationship['foreignKey'];
-            $this->ownerKey = $relationship['ownerKey'];
-            $this->fkName = $relationship['fkName'];
-            $this->snakeName = $relationship['snakeName'];
-        }
+  /**
+   * OdataRelationship constructor.
+   * @param array $relationship
+   */
+  public function __construct($relationship = [])
+  {
+    if ($relationship) {
+      $this->name = $relationship['name'];
+      $this->type = $relationship['type'];
+      $this->model = $relationship['model'];
+      $this->foreignKey = $relationship['foreignKey'];
+      $this->ownerKey = $relationship['ownerKey'];
+      $this->fkName = $relationship['fkName'];
+      $this->snakeName = $relationship['snakeName'];
     }
+  }
 }
